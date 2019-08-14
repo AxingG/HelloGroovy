13407f9ecfcc2f0d624a38491c2103d5ea674b19
AxingG <zhaojiaxing_name@163.com>
2019-08-14 15:21:23 +0800

    Initial commit

---
+++ src/HelloGroovy.groovy
@@ -0,0 +1,1 @@
+println 'Hello Groovy,老婆是李敏哦'
---
+++ src/variable/VariableStudy.groovy
@@ -0,0 +1,13 @@
+package variable
+
+String txt = '1111'
+String txt1 = '''
+st1
+st2
+st3
+'''
+String txt2 = "$txt $txt1 ${1 + 2 + 3 + 4}"
+def str = "groovy";
+println str.center(8,"a") // 先右再左
+println str.reverse()
+println str.capitalize()
