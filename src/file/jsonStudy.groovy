9a62da042a104891f3c9a311bdf939ad08a09583
AxingG <zhaojiaxing_name@163.com>
2019-08-15 08:19:24 +0800

    groovy 处理json

--- src/file/jsonStudy.groovy
+++ src/file/jsonStudy.groovy
@@ -1,2 +1,20 @@
 package file
 
+import groovy.json.JsonSlurper
+
+def response = getNetworkData('http://yuexibo.top/yxbApp/course_detail.json')
+
+println response.data.head.name
+
+def getNetworkData(String url) {
+    // 发送http请求
+    def connection = new URL(url).openConnection()
+    connection.setRequestMethod('GET')
+    connection.connect()
+    def response = connection.content.text
+
+    // 将json转化为实体对象
+    def jsonSlurper = new JsonSlurper();
+    return jsonSlurper.parseText(response)
+
+}
