162e17fc1ff5bb538bc4d9bc852bbaf513ea27b4
AxingG <zhaojiaxing_name@163.com>
2019-08-15 08:11:37 +0800

    通过闭包，仿gradle 引用

---
+++ src/datastruct/test/Gradle.groovy
@@ -0,0 +1,28 @@
+package datastruct.test
+
+class Gradle {
+    Set<String> api = new HashSet<>()
+
+    void api(String txt) {
+        api.add(txt)
+    }
+
+    void exc() {
+        println api.toListString()
+    }
+
+     static void dependencies(Closure closure) {
+        Gradle gradle = new Gradle();
+        closure.delegate = gradle
+        closure.call()
+        gradle.exc()
+    }
+
+    static void main(String[] args) {
+        dependencies {
+            api 'cn.jiguang.sdk.plugin:xiaomi:3.1.0'
+            api 'cn.jiguang.sdk.plugin:huawei:3.1.0'
+            api 'cn.jiguang.sdk.plugin:meizu:3.1.0'
+        }
+    }
+}
---
+++ src/datastruct/test/RunGradle.groovy
@@ -0,0 +1,5 @@
+package datastruct.test
+
+class RunGradle {
+
+}
